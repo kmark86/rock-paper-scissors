ed470a7d994971706ac9ae8e3e6f3c733444e877
kmark86 <73257505+kmark86@users.noreply.github.com>
2023-08-26 19:29:42 -0400

    Add getComputerChoice()

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello")
+function getComputerChoice(){
+    var choiceArray = ['Rock','Paper','Scissors'];
+    computerChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)]; //choiceArray[x] x = 0,1,2
+    //Math.random generates number between 0-1 (inclusive of 0, exclusive of 1)
+        // don't need 1 cause only need up to index of 2
+    //Math.floor rounds number down to floor.
+    return computerChoice
+}
+
+console.log(getComputerChoice())
